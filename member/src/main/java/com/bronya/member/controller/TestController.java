94f8894d832934a2f2b2880acfc7395409e31f70
bronya <2354796263@qq.com>
2024-04-23 15:37:30 +0800

    feat:增加aop日志切面

--- member/src/main/java/com/bronya/member/controller/TestController.java
+++ member/src/main/java/com/bronya/member/controller/TestController.java
@@ -1,22 +1,12 @@
 package com.bronya.member.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 〈一句话功能简述〉<br>
- * 〈〉
- *
- * @author bronya
- * @create 2024/4/23
- * @since 1.0.0
- */
 @RestController
-
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
-        return "hello";
-}
-
+        return "Hello World!";
+    }
 }
